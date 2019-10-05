8025c68b9848d7f4751d84d316eb402f248af9e8
Ailton Lima <ailtonlm.andrade@outlook.com>
2019-10-05 12:39:18 -0300

    Navbar e Favicon

--- js/script.js
+++ js/script.js
@@ -14,38 +14,32 @@ function scroll() {
 }
 
 /*================== Navegação ==================*/
-function navegar(){
-$('.scrollDirect').bind('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top
-            }, 1000, 'swing');
-        });
+function navegar() {
+    $('.scrollDirect').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'swing');
+    });
 }
 
 /*** Galeria ****/
 
-$(document).ready(function(){
-
-    $(".filter-button").click(function(){
+$(document).ready(function () {
+    $(".filter-button").click(function () {
         var value = $(this).attr('data-filter');
-        
-        if(value == "all")
-        {
-            //$('.filter').removeClass('hidden');
+
+        if (value == "all") {
             $('.filter').show('1000');
-        }
-        else
-        {
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
-            
+        } else {
+            $(".filter").not('.' + value).hide('3000');
+            $('.filter').filter('.' + value).show('3000');
         }
     });
-    
+
     if ($(".filter-button").removeClass("active")) {
-$(this).removeClass("active");
-}
-$(this).addClass("active");
+        $(this).removeClass("active");
+    }
+    $(this).addClass("active");
 
 });
