77f8ca8b54d06be9680bab50f36c61cf64f1eb5c
alandradeti <47827376+alandradeti@users.noreply.github.com>
2019-10-05 02:05:14 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/*======================================= Barra de Navegação - Scroll =======================================*/
+/*================== Scroll ==================*/
+window.onscroll = function () {
+    navegar();
+    scroll();
+};
+
+function scroll() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-100%";
+    }
+}
+
+/*================== Navegação ==================*/
+function navegar(){
+$('.scrollDirect').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1000, 'swing');
+        });
+}
+
+/*** Galeria ****/
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
