511820aa48ba7e7cf22b3754d70f89525e1dd2cc
Ailton Lima <ailtonlm.andrade@outlook.com>
2019-11-30 13:31:58 -0300

    Atualização

--- js/script.js
+++ js/script.js
@@ -8,8 +8,10 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if($(window).scrollTop()>300){
             $('nav').addClass('colorNav');
+            $('#logo').css('opacity', '1');
         }else{
-            $('nav').removeClass('colorNav');
+            $('nav').removeClass('colorNav'); 
+            $('#logo').css('opacity', '0');
         }
     })
 });
@@ -38,9 +40,9 @@ $(document).ready(function () {
         }
     });
 
-    if ($(".filter-button").removeClass("active")) {
+    /*if ($(".filter-button").removeClass("active")) {
         $(this).removeClass("active");
     }
-    $(this).addClass("active");
+    $(this).addClass("active");*/
 
 });
