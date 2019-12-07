1c95455956ce2c91b4af3da8917a0c6a152c299c
Ailton Lima <ailtonlm.andrade@outlook.com>
2019-12-06 23:03:48 -0300

    Update informacoes.js

--- classes/Support/informacoes.js
+++ classes/Support/informacoes.js
@@ -2,7 +2,7 @@ $(function(){
     $('.contatos').submit(function(){
         $(".loading").css("display", "inline-block");
         $.ajax({
-            url: 'https://alandradetiemail.000webhostapp.com/classes/Support/send-form.php',
+            url: 'classes/Support/send-form.php',
             type: 'POST',
             data: $('.contatos').serialize(),
             success: function(data){
