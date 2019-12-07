6c4ec0296f6996a2ad1cb67faa6a70620d99f1de
Ailton Lima <ailtonlm.andrade@outlook.com>
2019-12-07 11:45:16 -0300

    PHP em outro dominio

--- js/informacoes.js
+++ js/informacoes.js
@@ -2,11 +2,13 @@ $(function(){
     $('.contatos').submit(function(){
         $(".loading").css("display", "inline-block");
         $.ajax({
-            url: 'classes/Support/send-form.php',
+            url: 'https://alandradetiemail.000webhostapp.com/classes/Support/send-form.php',
+            //url: 'classes/Support/send-form.php',
             type: 'POST',
             data: $('.contatos').serialize(),
             success: function(data){
                 $('.mostrar').html(data);
+                $('.loading').hide();
                 $(".loading").css("display", "none");
 				$('.contatos')[0].reset();
             }
