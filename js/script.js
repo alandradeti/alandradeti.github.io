61753a4bc0cfc8cd1bc04d2e1fe852eeb4e990ec
Ailton Lima <ailtonlm.andrade@outlook.com>
2019-12-08 16:03:45 -0300

    Navbar responsiva

--- js/script.js
+++ js/script.js
@@ -3,16 +3,52 @@ window.onscroll = function () {
     navegar();
 };
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop()>300){
-            $('nav').addClass('colorNav');
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300 && window.innerWidth >= 770) {
+            $('.topnav').css('background', '#192231');
             $('#logo').css('opacity', '1');
-        }else{
-            $('nav').removeClass('colorNav'); 
+        } else if ($(window).scrollTop() < 300 && window.innerWidth >= 770) {
+            $('.topnav').css('background', 'rgba(0, 0, 0, 0.2)');
             $('#logo').css('opacity', '0');
         }
-    })
+    });
+});
+
+$(document).ready(function () {
+    if (window.innerWidth < 770) {
+        $('.topnav').css('background', '#192231');
+        $('#logo').css('opacity', '1');
+    }
+    $(window).resize(function () {
+        if (window.innerWidth < 770) {
+            $('.topnav').css('background', '#192231');
+            $('#logo').css('opacity', '1');
+        }
+    });
+});
+
+/*================== Barra de Navegação ==================*/
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+$(document).ready(function () {
+    $("#teste").click(function () {
+        if ($('#icone').hasClass('fa fa-bars')) {
+            $('#icone').removeClass('fa fa-bars');
+            $('#icone').addClass('fas fa-times');
+        } else {
+            $('#icone').removeClass('fas fa-times');
+            $('#icone').addClass('fa fa-bars');
+        }
+    });
 });
 
 /*================== Navegação ==================*/
